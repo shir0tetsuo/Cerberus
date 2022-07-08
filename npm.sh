b2a31003f4da98207babf6795f3e5fa08b244dca
shir0tetsuo <shadowsword@protonmail.com>
2022-07-07 20:49:55 -0400

    commit zero

---
+++ npm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#npm i -g npm
+#rm -r node_modules/
+#rm package-lock.json
+#npm cache clean --force
+#npm prune
+#npm update
+#npm install npm@latest -g
+
+npm init
+
+npm install --save discord.js
+#npm install --save canvas
+#npm install --save pg
+#npm install --save sqlite
+npm install --save sqlite3
+npm install --save chalk
+npm install --save sequelize
+npm install --save moment
+npm install --save canvas
+npm install --save dotenv
